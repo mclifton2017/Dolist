8a10ba22ea952cee67d7775e18c750e01c1640c8
McShane Clifton <mcshaneclifton@mcshanes-macbook-pro.local>
2018-11-28 21:59:55 -0800

    Initial Commit

---
+++ Dolist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dolist
+//
+//  Created by McShane Clifton on 11/28/18.
+//  Copyright © 2018 McShane Clifton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
